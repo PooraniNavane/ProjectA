19b957d673f677f2bb4c3a16909b32d25bc2f643
PooraniNavane <star.navane@gmail.com>
2023-02-02 20:19:44 +0530

    newcodeadded

---
+++ DealerNameAddress.java
@@ -0,0 +1,21 @@
+package dealerdetails;
+
+public class DealerNameAddress {
+	
+	private void dealerNamePhoneNumber() {
+		System.out.println("Enter the Dealar Name");
+		System.out.println("Enter Tha PhoneNumber");
+		
+
+	} 
+	private void dealerAddress() {
+		System.out.println("Enter Tha Address");
+
+	}
+	public static void main(String[] args) {
+		DealerNameAddress n = new DealerNameAddress();
+		n.dealerNamePhoneNumber();
+		n.dealerAddress();
+	}
+
+}
